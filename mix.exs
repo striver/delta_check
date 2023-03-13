751f13492c08a4e4f380e3c28bfca32b449665c9
Andreas Geffen Lundahl <andreas@geffenlundahl.com>
2023-03-13 17:05:17 +0100

    Release 0.1.1

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule DeltaCheck.MixProject do
       ],
       source_url: "https://github.com/striver/delta_check",
       start_permanent: Mix.env() == :prod,
-      version: "0.1.0"
+      version: "0.1.1"
     ]
   end
 
